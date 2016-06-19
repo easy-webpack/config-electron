77e8793afe028d99548a85a9ffb688b523739fc3
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 22:00:51 +0200

    feat(all): initial commit

---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import {WebpackConfig, get, literalReplace} from '@easy-webpack/core'
+const DefinePlugin = require('webpack/lib/DefinePlugin')
+
+/**
+ * @param externals list packages that should be used as node modules, directly from node_modules (without bundling)
+ */
+export function electron(externals: Array<string>) {
+  return function electron(this: WebpackConfig): WebpackConfig {
+    return {
+      output: literalReplace<any>({
+        path: this.output.path,
+        filename: 'bundle.js',
+        libraryTarget: 'commonjs2'
+      }),
+      
+      resolve: {
+        packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
+        mainFields: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
+      },
+
+      plugins: [
+        new DefinePlugin({
+          '__DEV__': true,
+          'ENV': JSON.stringify(this.metadata.ENV),
+          'HMR': this.metadata.HMR,
+          'process.env': {
+            'ENV': JSON.stringify(this.metadata.ENV),
+            'NODE_ENV': JSON.stringify(this.metadata.ENV),
+            'HMR': this.metadata.HMR,
+          }
+        })
+      ].concat(get(this, 'plugins', [])),
+
+      externals: externals.concat(get(this, 'externals', []))
+    }
+  }
+}
